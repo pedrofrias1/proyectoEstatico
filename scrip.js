b821d32815271cd7c3773b77be53edecf51eec4b
pedro frias <pedrotomasfrias8@gmail.com>
2023-08-04 12:13:09 -0300

    nuevo github

---
+++ scrip.js
@@ -0,0 +1,16 @@
+const valueDisplay=document.querySelectorAll(".num");
+let interval= 5000;
+var start= 21;
+
+valueDisplay.forEach((valueDisplay) => {
+    let startValue= 0;
+    let endValue= parseInt(valueDisplay.getAttribute ("data-val"));
+    let duration= Math.floor(interval/endValue);
+    let counter= setInterval(function () {
+        startValue +=1;
+        valueDisplay.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
