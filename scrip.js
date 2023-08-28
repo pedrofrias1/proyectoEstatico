1e3edd3e153e26f536ac6f3db7111411442d4365
pedro frias <pedrotomasfrias1@gmail.com>
2023-08-28 17:06:17 -0300

    finalizada

--- scrip.js
+++ scrip.js
@@ -1,18 +1,20 @@
-// const valueDisplay=document.querySelectorAll(".num");
-// let interval= 5000;
-// var start= 21;
+const valueDisplay=document.querySelectorAll(".num");
+let interval= 5000;
+var start= 21;
 
-// valueDisplay.forEach((valueDisplay) => {
-//     let startValue= 0;
-//     let endValue= parseInt(valueDisplay.getAttribute ("data-val"));
-//     let duration= Math.floor(interval/endValue);
-//     let counter= setInterval(function () {
-//         startValue +=1;
-//         valueDisplay.textContent = startValue;
-//         if(startValue == endValue){
-//             clearInterval(counter);
-//         }
-//     }, duration);
-// });
+valueDisplay.forEach((valueDisplay) => {
+    let startValue= 0;
+    let endValue= parseInt(valueDisplay.getAttribute ("data-val"));
+    let duration= Math.floor(interval/endValue);
+    let counter= setInterval(function () {
+        startValue +=1;
+        valueDisplay.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
+
 
 
